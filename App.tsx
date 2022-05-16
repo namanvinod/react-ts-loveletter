91012802264a5c773842ee96669251b3a93c7447
namanvinod <namanvinod1@gmail.com>
2022-05-16 22:06:43 +0530

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Dashboard from './components/Dashboard/Dashboard';
 import Login from './components/Login/Login';
+
 import './style.css';
 
 export default function App() {
