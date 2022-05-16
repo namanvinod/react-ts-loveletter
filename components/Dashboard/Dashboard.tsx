1d1486743db9bd446834fd6995aabc01848fea8e
namanvinod <namanvinod1@gmail.com>
2022-05-16 22:11:01 +0530

    2 files modified

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Dashboard = () => {
-  return 'Dashboard';
+  return (
+    <Fragment>
+      <div className="dashboard-container">
+        <button>Create New Game</button>
+        <button>Join A Game</button>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Dashboard;
