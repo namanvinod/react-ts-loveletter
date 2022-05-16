e14ebc880de37007e53c21f642851fab16f6fcb0
namanvinod <namanvinod1@gmail.com>
2022-05-16 22:08:05 +0530

    1 file modified

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -1,9 +1,13 @@
 import React, { Fragment } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import './Login.css';
 
 const Login = () => {
+  const navigate = useNavigate();
+
   const handleLogin = () => {
-    console.log('Test');
+    navigate('/dashboard');
   };
 
   return (
